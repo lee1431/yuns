7bb2462bf44bfac10afed4c9d577bd896f4a2f9a
lee1431 <37124343+lee1431@users.noreply.github.com>
2026-08-22 18:10:16 +0900

    Change STUDIO_API_BASE to public HTTPS address

--- assets/config.js
+++ assets/config.js
@@ -1,3 +1,3 @@
 // 스마트폰 백엔드의 공개 HTTPS 주소로 변경하세요.
 // 예: https://studio-api.example.com
-window.STUDIO_API_BASE = "http://127.0.0.1:8665";
+window.STUDIO_API_BASE = "https://mrdindoin.ddns.net/yuns/";
